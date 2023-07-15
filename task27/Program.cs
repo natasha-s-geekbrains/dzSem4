967cb6b9e7947b580a7e813e8d69149f4c15fe3b
natasha_s <natasl1012@gmail.com>
2023-07-15 22:51:21 +0300

    added task 27

---
+++ task27/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 27: Напишите программу, которая принимает на 
+// вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Prompt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int GetDigitSum(int number)
+{
+    int digitSum = 0;
+
+    if (number < 10)
+    {
+        digitSum = number;
+    }
+    else
+    {
+        for (int i = number; i >= 10; i /= 10)
+        {
+            digitSum += i % 10;
+            number = i/10;
+        }
+        digitSum = digitSum + number;
+    }
+    return digitSum;
+}
+
+int num = Prompt("Введите число: ");
+GetDigitSum(num);
+Console.WriteLine($"{num} -> {GetDigitSum(num)}");
+
